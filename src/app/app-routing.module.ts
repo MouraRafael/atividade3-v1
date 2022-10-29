a1edb5a327590ab06a3cdb17419ff26cd2b43f9b
Rafael Moura <rafael.g.moura@hotmail.com>
2022-10-29 20:45:07 -0300

    feat:rota cadastro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastrarComponent } from './estoque/components/cadastrar/cadastrar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'estoque/cadastro',
+    component: CadastrarComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
