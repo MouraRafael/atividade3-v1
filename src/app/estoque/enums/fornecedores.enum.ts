7c7dc020f2243ca4925d65b4916789b4236f9a08
Rafael Moura <rafael.g.moura@hotmail.com>
2022-10-30 19:16:18 -0300

    fix: fornecedor Label

--- src/app/estoque/enums/fornecedores.enum.ts
+++ src/app/estoque/enums/fornecedores.enum.ts
@@ -6,10 +6,10 @@ export enum EstoqueFornecedores{
 }
 
 export const EstoqueFornecedoresLabel = new Map<number,string>([
-  [
+
     [EstoqueFornecedores.JOAO_BARROS,"Frigorífico João Barros"],
     [EstoqueFornecedores.MXS_VEGETAIS, "MxS Vegetais Frescos"],
     [EstoqueFornecedores.FALLIN_IMPORTADOS,"Fall In Importados Ltda."],
     [EstoqueFornecedores.DEMETER_LATICINIOS,"Demeter Laticínios & Mantimentos"]
-  ]
+
 ])
