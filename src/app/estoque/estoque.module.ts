8c66c2e88f36b6323acf8c5c38816698de915fa7
Rafael Moura <rafael.g.moura@hotmail.com>
2022-10-29 22:22:52 -0300

    feat: imports

--- src/app/estoque/estoque.module.ts
+++ src/app/estoque/estoque.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CadastrarComponent } from './components/cadastrar/cadastrar.component';
+
 import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { MatButtonModule } from "@angular/material/button"
+
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
-
+const materialModules = [
+  ReactiveFormsModule,
+  MatFormFieldModule,
+  MatButtonModule,
+  MatInputModule
+]
 
 @NgModule({
   declarations: [CadastrarComponent],
@@ -12,7 +21,7 @@ import { FormsModule, ReactiveFormsModule } from "@angular/forms";
     CommonModule,
     MatFormFieldModule,
     FormsModule,
-    ReactiveFormsModule
+    ...materialModules,
   ],
   exports: [CadastrarComponent]
 })
