e4396c86bf7c87f1ad78e53dbba62d3c9ced2448
Rafael Moura <rafael.g.moura@hotmail.com>
2022-11-02 20:01:51 -0300

    feat: refatoracao do menu + Router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EstoqueModule } from './estoque/estoque.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     AppRoutingModule,
     BrowserAnimationsModule,
     EstoqueModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
