ba7b10cc46e6e8893df23fb4a46ba258c491de6e
Rafael Moura <rafael.g.moura@hotmail.com>
2022-11-06 11:08:54 -0300

    feat: editar funcionando

--- src/app/estoque/components/editar/editar.component.ts
+++ src/app/estoque/components/editar/editar.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { EstoqueCategoria } from '../../enums/categoria.enum';
+import { EstoqueFornecedores } from '../../enums/fornecedores.enum';
 import { ProdutoModel } from '../../model/produto.model';
 import { EstoqueService } from '../../services/estoque.service';
 
@@ -9,16 +12,59 @@ import { EstoqueService } from '../../services/estoque.service';
   styleUrls: ['./editar.component.css']
 })
 export class EditarComponent implements OnInit {
+  categorias = [
+    [EstoqueCategoria.CARNES,"Carnes"],
+    [EstoqueCategoria.FRUTAS,"Frutas"],
+    [EstoqueCategoria.LEGUMES,"Legumes"],
+    [EstoqueCategoria.VEGETAIS,"Vegetais"],
+    [EstoqueCategoria.FRIOS,"Frios"],
+    [EstoqueCategoria.LATICINIOS,"Laticínio"]
+  ]
+  fornecedores = [
+    [EstoqueFornecedores.JOAO_BARROS,"Frigorífico João Barros"],
+    [EstoqueFornecedores.MXS_VEGETAIS, "MxS Vegetais Frescos"],
+    [EstoqueFornecedores.FALLIN_IMPORTADOS,"Fall In Importados Ltda."],
+    [EstoqueFornecedores.DEMETER_LATICINIOS,"Demeter Laticínios & Mantimentos"]
+  ]
+
+
+
   itemEstoque!:ProdutoModel;
+  produtoEditarForm!:FormGroup;
+
 
   constructor(
     private estoqueService: EstoqueService,
-    private route:ActivatedRoute
+    private route:ActivatedRoute,
+    private formBuilder:FormBuilder
   ) { }
 
   ngOnInit(): void {
     const id = this.route.snapshot.params['id'];
     this.itemEstoque = this.estoqueService.localizarPorID(id);
+
+    this.produtoEditarForm = this.formBuilder.group({
+      nome:[this.itemEstoque.nome,[Validators.required,Validators.pattern(/^[a-zA-Z]/)]],
+      quantidade:[this.itemEstoque.quantidade,[Validators.required,Validators.pattern(/[0-9]/)]],
+      fornecedor:[this.itemEstoque.fornecedor,[Validators.required]],
+      validade:[this.itemEstoque.validade,[Validators.required]],
+      categoria:[this.itemEstoque.categoria,[Validators.required]],
+
+      })
   }
+
+
+  editar(){
+    this.itemEstoque.nome = this.produtoEditarForm.get("nome")!.value;
+    this.itemEstoque.quantidade = this.produtoEditarForm.get("quantidade")!.value;
+    this.itemEstoque.fornecedor = this.produtoEditarForm.get("fornecedor")!.value;
+    this.itemEstoque.validade = this.produtoEditarForm.get("validade")!.value;
+    this.itemEstoque.categoria = this.produtoEditarForm.get("categoria")!.value;
+
+    this.estoqueService.editar(this.itemEstoque)
+  }
+
+  get nomeProduto() {return this.produtoEditarForm.get("nome")!}
+  get categoria() {return this.produtoEditarForm.get("categoria")!}
 
 }
